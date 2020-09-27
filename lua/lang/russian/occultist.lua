08e2ffc35a72d102f67f8482c01ecf1a9ddccc06
Blueberryy <igory.ygr200@gmail.com>
2020-09-27 20:28:59 +0500

    Create occultist.lua

---
+++ lua/lang/russian/occultist.lua
@@ -0,0 +1,14 @@
+L = LANG.GetLanguageTableReference("Русский")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[OCCULTIST.name] = "Оккультист"
+L["info_popup_" .. OCCULTIST.name] = [[Вы оккультист!
+Вы успешно выполнили ритуал. Теперь вы можете вернуться к жизни, когда ваш здоровья упадёт ниже 25ед.
+Если вы получите один выстрел, ваш ритуал не подействует.]]
+L["body_found_" .. OCCULTIST.abbr] = "Он был оккультистом."
+L["search_role_" .. OCCULTIST.abbr] = "Этот человек был оккультистом!"
+L["target_" .. OCCULTIST.name] = "Оккультист"
+L["ttt2_desc_" .. OCCULTIST.name] = [[Оккультисту нужно побеждать невинных!]]
+
+-- OTHER ROLE LANGUAGE STRINGS
+L["ttt2_role_occultist_popuptitle"] = "Как феникс, оккультист восстаёт из своего пылающего ада!"
